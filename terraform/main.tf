4975fcd48741faf73f2b4d60421bc4c6ac2cc1ec
ziazeshan141 <ziazeshan141@outlook.com>
2026-08-04 11:50:55 +0300

    Updated again the main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,14 +15,9 @@ provider "aws" {
 # ------------------------------------------------------------------------------
 # Data Sources
 # ------------------------------------------------------------------------------
+# Standard, working data source definition:
 data "aws_availability_zones" "available" {
   state = "available"
-  
-# Exclude Local Zones / Wavelength zones that might mess up standard VPC subnets
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
 }
 
 # ------------------------------------------------------------------------------
@@ -35,7 +30,11 @@ module "vpc" {
   name = "${var.cluster_name}-vpc"
   cidr = var.vpc_cidr
 
-  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs = slice(
+    data.aws_availability_zones.available.names,
+     0,
+     min(3, length(data.aws_availability_zones.available.names))
+    )
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
