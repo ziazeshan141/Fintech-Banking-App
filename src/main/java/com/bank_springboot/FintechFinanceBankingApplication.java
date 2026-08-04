ba856fe7798cb850c90acec49611e889afbdc6c1
ziazeshan141 <ziazeshan141@outlook.com>
2026-08-04 20:28:52 +0300

    update

--- src/main/java/com/bank_springboot/FintechFinanceBankingApplication.java
+++ src/main/java/com/bank_springboot/FintechFinanceBankingApplication.java
@@ -4,17 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-// import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @SpringBootApplication
 public class FintechFinanceBankingApplication extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
-		return applicationBuilder.sources(FintechFinanceBankingApplication.class);
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(FintechFinanceBankingApplication.class, args);
 
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FintechFinanceBankingApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(FintechFinanceBankingApplication.class, args);
+    }
 }
