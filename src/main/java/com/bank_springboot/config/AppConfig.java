8315156857ff0347750afb3ae305fd8829a441a6
ziazeshan141 <ziazeshan141@outlook.com>
2026-08-04 22:43:19 +0300

    Updated the file

--- src/main/java/com/bank_springboot/config/AppConfig.java
+++ src/main/java/com/bank_springboot/config/AppConfig.java
@@ -1,42 +1,23 @@
 package com.bank_springboot.config;
 
-import com.bank_springboot.interceptors.AppInterceptor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@ComponentScan(basePackages = {"com.bank_springboot"})
-public class AppConfig extends WebMvcConfigurationSupport {
-
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("css/**","images/**", "js/**")
-                .addResourceLocations("classpath:/static/css/", "classpath:/static/images/", "classpath:/static/js/");
-    }
-    // End Of Resource Handler.
+public class AppConfig {
 
     @Bean
-    public InternalResourceViewResolver viewResolver(){
-        InternalResourceViewResolver jspViewResolver = new InternalResourceViewResolver();
-        jspViewResolver.setPrefix("/WEB-INF/jsp/");
-        jspViewResolver.setSuffix(".jsp");
-        jspViewResolver.setViewClass(JstlView.class);
+    public InternalResourceViewResolver viewResolver() {
 
-        return jspViewResolver;
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver();
+
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
     }
-    // End Of View Resolver.
-
-
-    @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AppInterceptor()).addPathPatterns("/app/*");
-        
-    }
-    // End Of Interceptor Registry.
 }
